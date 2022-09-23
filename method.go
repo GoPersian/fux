b1fd8054226f0f979d4abc929f433c34646a1f4e
Kamandlou <ahkamandlou@gmail.com>
2022-09-23 16:00:28 +0330

    update http methods

--- method.go
+++ method.go
@@ -1,27 +1,30 @@
 package fux
 
-import "net/http"
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
 
 type (
 	Handler func(w http.ResponseWriter, request *http.Request)
 )
 
-func (f *Fux) Get(pattern string, handler Handler) {
-	f.Router.HandleFunc(pattern, handler).Methods(http.MethodGet)
+func (f *Fux) Get(pattern string, handler Handler) *mux.Route {
+	return f.Router.HandleFunc(pattern, handler).Methods(http.MethodGet)
 }
 
-func (f *Fux) Post(pattern string, handler Handler) {
-	f.Router.HandleFunc(pattern, handler).Methods(http.MethodPost)
+func (f *Fux) Post(pattern string, handler Handler) *mux.Route {
+	return f.Router.HandleFunc(pattern, handler).Methods(http.MethodPost)
 }
 
-func (f *Fux) Put(pattern string, handler Handler) {
-	f.Router.HandleFunc(pattern, handler).Methods(http.MethodPut)
+func (f *Fux) Put(pattern string, handler Handler) *mux.Route {
+	return f.Router.HandleFunc(pattern, handler).Methods(http.MethodPut)
 }
 
-func (f *Fux) Patch(pattern string, handler Handler) {
-	f.Router.HandleFunc(pattern, handler).Methods(http.MethodPatch)
+func (f *Fux) Patch(pattern string, handler Handler) *mux.Route {
+	return f.Router.HandleFunc(pattern, handler).Methods(http.MethodPatch)
 }
 
-func (f *Fux) Delete(pattern string, handler Handler) {
-	f.Router.HandleFunc(pattern, handler).Methods(http.MethodDelete)
+func (f *Fux) Delete(pattern string, handler Handler) *mux.Route {
+	return f.Router.HandleFunc(pattern, handler).Methods(http.MethodDelete)
 }
